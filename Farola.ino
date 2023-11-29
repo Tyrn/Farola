088a3645472aef6a222fb950e3f1b8a0f57c1ecc
Tyrn <a@b.com>
2023-11-29 17:36:41 +0300

    Scheduled Christmas Tree enabled

--- Farola.ino
+++ Farola.ino
@@ -2,21 +2,52 @@
 #define _LCD_TYPE 1
 #include <LCD_1602_RUS_ALL.h>
 
+#define _TASK_SCHEDULING_OPTIONS
+// #define _TASK_TIMECRITICAL
+// #define _TASK_SLEEP_ON_IDLE_RUN
+#include <TaskScheduler.h>
+
 LCD_1602_RUS lcd(0x27, 16, 2);
+
+void tGreenCallback() { digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); }
+
+void tOrangeCallback() { digitalWrite(PB8, !digitalRead(PB8)); }
+
+void tCountCallback() {
+  static int cnt = 1;
+
+  lcd.setCursor(0, 0);
+  lcd.print(cnt++, DEC);
+}
+
+Task tGreen(200, TASK_FOREVER, &tGreenCallback);
+Task tOrange(500, TASK_FOREVER, &tOrangeCallback);
+Task tCount(1000, TASK_FOREVER, &tCountCallback);
+
+Scheduler ts;
 
 void setup() {
   String str;
   str = ". Hexadecimal";
-
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PB8, OUTPUT);
   lcd.init(); // For the Blue Pill use LCD_1602_RUS_ALL fork.
 
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print(1, DEC);
-  lcd.print(". Йа креведко!");
-  lcd.setCursor(0, 1);
-  lcd.print(15, HEX);
-  lcd.print(str);
+  // lcd.print(1, DEC);
+  // lcd.print(". Йа креведко!");
+  // lcd.setCursor(0, 1);
+  // lcd.print(15, HEX);
+  // lcd.print(str);
+
+  ts.init();
+  ts.addTask(tGreen);
+  tGreen.enable();
+  ts.addTask(tOrange);
+  tOrange.enable();
+  ts.addTask(tCount);
+  tCount.enable();
 }
 
-void loop() {}
+void loop() { ts.execute(); }
