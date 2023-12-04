ef7c9f85318359c2d39941732c917aafc794e843
Tyrn <a@b.com>
2023-12-04 17:14:44 +0300

    Tasks tOrange2 and tEncoder added

--- Farola.ino
+++ Farola.ino
@@ -12,9 +12,15 @@
 
 LCD_1602_RUS lcd(0x27, 16, 2);
 
+int32_t prevPos = 0;
+int16_t myVar = 64;
+int16_t prevmyVar = myVar;
+
 void tGreenCallback() { digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); }
 
-void tOrangeCallback() { digitalWrite(PB8, !digitalRead(PB8)); }
+void tOrange1Callback() { digitalWrite(PB8, !digitalRead(PB8)); }
+
+void tOrange2Callback() { digitalWrite(PB9, !digitalRead(PB9)); }
 
 void tCountCallback() {
   static int cnt = 1;
@@ -23,20 +29,31 @@ void tCountCallback() {
   lcd.print(cnt++, DEC);
 }
 
+void tEncoderCallback() {
+  lcd.setCursor(0, 0);
+  lcd.print(prevPos, DEC);
+}
+
+Task tEncoder(100, TASK_FOREVER, &tEncoderCallback);
 Task tGreen(200, TASK_FOREVER, &tGreenCallback);
-Task tOrange(500, TASK_FOREVER, &tOrangeCallback);
+Task tOrange1(500, TASK_FOREVER, &tOrange1Callback);
+Task tOrange2(700, TASK_FOREVER, &tOrange2Callback);
 Task tCount(1000, TASK_FOREVER, &tCountCallback);
 
 Scheduler ts;
-STM32encoder enc(TIM2);
+STM32encoder enc(TIM2, 0x07);
 
 void setup() {
   String str;
   str = ". Hexadecimal";
 
-  Serial.begin(115200);
+  // Serial.begin(115200);
+  enc.bind(&myVar);
+  enc.dynamic(20, 30, false);
+
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PB8, OUTPUT);
+  pinMode(PB9, OUTPUT);
   lcd.init(); // For the Blue Pill use LCD_1602_RUS_ALL fork.
 
   lcd.backlight();
@@ -48,10 +65,14 @@ void setup() {
   // lcd.print(str);
 
   ts.init();
+  ts.addTask(tEncoder);
+  tEncoder.enable();
   ts.addTask(tGreen);
   tGreen.enable();
-  ts.addTask(tOrange);
-  tOrange.enable();
+  ts.addTask(tOrange1);
+  tOrange1.enable();
+  ts.addTask(tOrange2);
+  tOrange2.enable();
   ts.addTask(tCount);
   tCount.enable();
 }
@@ -60,6 +81,20 @@ void loop() {
   ts.execute();
 
   if (enc.isUpdated()) {
-    Serial.printf("pos:%ld\n", enc.pos());
+    // Serial.printf("pos:%ld\n", enc.pos());
+    // lcd.setCursor(0, 0);
+    // lcd.printf("pos:%ld", (long)enc.pos());
+    // lcd.print(enc.pos(), DEC);
+    int32_t pos = enc.pos();
+    // Serial.printf(
+    //     "pos:%5ld myVar:%5d speed:%2d pos delta:%2ld myVar delta:%3ld\n"
+    //     , pos
+    //     , myVar
+    //     , enc.speed()
+    //     , pos - prevPos
+    //     , myVar - prevmyVar
+    // );
+    prevPos = pos;
+    prevmyVar = myVar;
   }
 }
